1f254a0aa9867ba3db38b6c051efed59213e705b
Pasha <pavel8lisenkov@yandex.ru>
2023-08-02 02:10:38 +0400

    create project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1134px',
+      },
+      colors: {
+        'gray-dark': '#B5B2B2',
+        'gray': '#E8E8E8',
+        'gray-light': '#F5F5F5',
+        'deep-purple': '#7840B0',
+      },
+      fontFamily: {
+        Roboto: 'Roboto'
+      }
+    },
+  },
+  plugins: [],
+}
+
